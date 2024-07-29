7bae5e10f2ab26332762ca015f3a2c6b4e6e520f
arunartificer <arunprasath.artificer@gmail.com>
2024-07-29 17:22:43 +0400

    update footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -6,25 +6,19 @@ const Footer = () => {
 
   return (
     <footer className='bg-gray-200 py-4 mt-24'>
-  <div className='container mx-auto flex flex-col md:flex-row items-center justify-between px-4'>
-    <div className='mb-4 md:mb-0 flex items-center'>
-      <Link href='https://www.aqaarplusae.com'>
-        <a>
-          <Image src={logo} alt='Logo' className='h-8 w-auto' height={40} quality={90} />
-        </a>
-      </Link>
-    </div>
+      <div className='container mx-auto flex flex-col md:flex-row items-center justify-between px-4'>
+        <div className='mb-4 md:mb-0'>
+          <Image src={logo} alt='Logo' className='h-8 w-auto' height={40} 
+    quality={90} />
+        </div>
 
-    <div className='text-center md:text-left'>
-      <p className='text-sm text-gray-500'>
-        &copy; {currentYear} Aqaar Pulse Real Estate Brokerage Co. LLC.
-        <br />
-        All rights reserved.
-      </p>
-    </div>
-  </div>
-</footer>
-
+        <div>
+          <p className='text-sm text-gray-500 mt-2 md:mt-0'>
+            &copy; {currentYear} AqaarPulse - All rights reserved.
+          </p>
+        </div>
+      </div>
+    </footer>
   );
 };
 export default Footer;
