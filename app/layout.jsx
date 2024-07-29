d797d93de9bdeba3e2bd803ca51798575db099bd
arunartificer <arunprasath.artificer@gmail.com>
2024-07-29 18:01:27 +0400

    rechange-revise

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,3 @@
-import Head from 'next/head';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 import AuthProvider from '@/components/AuthProvider';
@@ -9,9 +8,9 @@ import 'react-toastify/dist/ReactToastify.css';
 import 'photoswipe/dist/photoswipe.css';
 
 export const metadata = {
-  title: 'AqaarPulse | Find The Perfect Rental and Sales Properties',
-  description: 'Find your dream rental property with AqaarPulse. Explore a wide range of rental and sales properties tailored to your needs.',
-  keywords: 'real estate, rental properties, sales properties, find rentals, property listings',
+  title: 'AqaarPulse | Find The Perfect Rental and Sales Properies',
+  description: 'Find your dream rental property',
+  keywords: 'rental, find rentals, find properties',
 };
 
 const MainLayout = ({ children }) => {
@@ -19,23 +18,15 @@ const MainLayout = ({ children }) => {
     <GlobalProvider>
       <AuthProvider>
         <html lang='en'>
-          <Head>
-            <title>{metadata.title}</title>
-            <meta name='description' content={metadata.description} />
-            <meta name='keywords' content={metadata.keywords} />
-            <link rel='icon' href='/assets/images/Favicon-aqaar-20.svg' />
-            {/* You can add more meta tags here if needed */}
-          </Head>
           <body>
             <Navbar />
             <main>{children}</main>
             <Footer />
-            <ToastContainer />
+            {/* <ToastContainer /> */}
           </body>
         </html>
       </AuthProvider>
     </GlobalProvider>
   );
 };
-
 export default MainLayout;
